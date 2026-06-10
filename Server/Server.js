8f51335b8b41c369c1e0fbbdbe55bc5016354936
Sarang Akhade <Akhadesarang1@gmail.com>
2026-06-10 10:57:50 +0530

    chore :  setup for upgradation of server
    Removed unnecessary blank lines for cleaner code.

--- Server/Server.js
+++ Server/Server.js
@@ -17,11 +17,6 @@ const fs = require("fs");
 
 dotenv.config();
 
-
-
-
-
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
